90d0231dac75e10dfc9f5f22383c38c67724649e
satvik9676 <2420080047@klh.edu.in>
2025-10-08 09:25:30 +0530

    Add files via upload

---
+++ week7/data.js
@@ -0,0 +1,6 @@
+export const books = [
+  { title: "Harry Potter", author: "ManiRao", price: 499, availability: "in stock" },
+  { title: "It Ends With Us", author: "S. Radhakrishnan", price: 499, availability: "in stock" },
+  { title: "Ugly Love", author: "New books edition", price: 499, availability: "out of stock" },
+  { title: "Regretting u", author: "Jaico Book House", price: 499, availability: "in stock" }
+];
